349fbca1769c2cd07c70b4914a180b3e51ca0dc8
Rizal-203040067 <rizal.baihaqi007@gmail.com>
2024-07-27 20:28:55 +0700

    menambahkan VideoSeeder

--- database/seeders/VideoSeeder.php
+++ database/seeders/VideoSeeder.php
@@ -2,16 +2,30 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Video;
 use Illuminate\Database\Seeder;
 
 class VideoSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
-    public function run(): void
+    public function run()
     {
-        //
+        Video::create([
+            'title' => 'Belajar Laravel 11 | 1. Intro',
+            'keyvideo' => 'T1TR-RGf2Pw',
+        ]);
+        Video::create([
+            'title' => 'Belajar Laravel 11 | 2. Instalasi & Konfigurasi',
+            'keyvideo' => 'nW60yGRoUrs',
+        ]);
+        Video::create([
+            'title' => 'Belajar Laravel 11 | 3. Struktur Folder',
+            'keyvideo' => 'x55ndgkD2QI',
+        ]);
+        Video::create([
+            'title' => 'Belajar Laravel 11 | 4. Blade Templating Engine',
+            'keyvideo' => 'vDx6VA-6a6Y',
+        ]);
+
+        // Tambahkan video lain sesuai kebutuhan
     }
 }
